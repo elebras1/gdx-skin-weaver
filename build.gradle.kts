e9d9287953d6354e8c3190601f75b1a4de9d5fe3
elebras1 <erwan.lebras27@gmail.com>
2026-05-31 16:36:30 +0200

    add sonatype url

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,12 +35,13 @@ gradlePlugin {
 nexusPublishing {
     repositories {
         sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
             username.set(System.getenv("OSSRH_USERNAME"))
             password.set(System.getenv("OSSRH_PASSWORD"))
         }
     }
 }
-
 publishing {
     publications {
         create<MavenPublication>("maven") {
